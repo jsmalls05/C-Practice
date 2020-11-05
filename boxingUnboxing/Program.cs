3cb5e51772d0de4c95c9185b8f4417550e512c0e
James Smalls <50281620+jsmalls05@users.noreply.github.com>
2020-11-05 17:35:23 -0500

    Add files via upload

---
+++ boxingUnboxing/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace boxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objList = new List<object>
+            {
+                7,
+                28,
+                -1,
+                true,
+                "chair"
+            };
+
+            foreach (var o in objList)
+            {
+                Console.WriteLine(o);
+            }
+            int sum = 0;
+            foreach (var o in objList)
+            {
+                if (o is int)
+                {
+                    sum += (int)o;
+                }
+
+
+            }
+                Console.WriteLine(sum);
+
+        }
+    }
+}
